23f3b7aa13459aa1758939326e6454ccd783a535
Sab1e-dev <sab1e@qq.com>
2026-04-20 13:31:18 +0800

    chore: Update base URL in Docusaurus configuration for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://sab1e-dev.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ElenaOS-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
